e88f4938749e493557678b77ddd95387d604885c
aldoBangun <punk.aldo68@gmail.com>
2022-10-04 22:08:53 +0800

    initial setup CRA + TS + MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Button from '@mui/material/Button'
+import SettingsOutlined from '@mui/icons-material/SettingsOutlined'
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button
+        startIcon={<SettingsOutlined />}
+        variant="outlined"
+        color="secondary"
+      >
+        Settings
+      </Button>
+      <Button
+        variant="text"
+      >
+        Logout
+      </Button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/theme.tsx
@@ -0,0 +1,16 @@
+import createTheme from '@mui/material/styles/createTheme'
+
+export default createTheme({
+  palette: {
+    primary: {
+      main: '#E94560'
+    },
+    secondary: {
+      main: '#533483',
+      dark: '#0F3460'
+    },
+    background: {
+      default: '#16213E'
+    }
+  }
+})
